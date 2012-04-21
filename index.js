b48ae116dd664214425114627c6906cc726275a8
Christopher Martin <ch.martin@gmail.com>
2012-04-20 23:57:46 -0400

    remove test stuff

--- index.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $.ajax('data/whatever.json', {
-    success: function(data) {
-      $.each(data.stuff, function(i, thing) {
-        $('<div/>').text(thing).appendTo('body');
-      });
-    }
-  });
-});

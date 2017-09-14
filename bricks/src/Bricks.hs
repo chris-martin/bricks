ddecace65027118abdcb185899348f7119ac85f8
Chris Martin <ch.martin@gmail.com>
2017-09-13 21:57:13 -0400

    export list for top-level Bricks module

--- bricks/src/Bricks.hs
+++ bricks/src/Bricks.hs
@@ -14,13 +14,158 @@ Notable differences from Nix:
 -}
 module Bricks
 
-  ( module Bricks.Expression
-  , module Bricks.IndentedString
-  , module Bricks.Keyword
-  , module Bricks.Parsing
-  , module Bricks.Rendering
-  , module Bricks.UnquotedString
-
+  (
+  -------------------------------------------------
+  -- * Expressions
+    Expression (..)
+  -- ** Rendering expressions
+  , render'expression
+  , render'expression'listContext
+  , render'expression'dotLeftContext
+  , render'expression'applyLeftContext
+  , render'expression'applyRightContext
+  , render'expression'inParens
+  , render'expression'dictKey
+  -- ** Parsing expressions
+  , parse'expression
+  , parse'expression'paren
+  , parse'expression'dictKey
+  -- ** Parsing lists of expressions
+  , parse'expressionList
+  , parse'expressionList'1
+  , parse'expressionList'1'noDot
+  -------------------------------------------------
+  -- * Strings
+  , str'escape
+  -- ** Static strings
+  , Str'Static
+  , render'strStatic'unquotedIfPossible
+  , render'strStatic'quoted
+  , parse'strStatic
+  , parse'strStatic'quoted
+  , parse'strStatic'unquoted
+  -- ** Dynamic strings
+  , Str'Dynamic
+  , Str'1 (..)
+  , render'strDynamic'unquotedIfPossible
+  , render'strDynamic'quoted
+  , parse'strDynamic'quoted
+  , parse'strDynamic'normalQ
+  , parse'strDynamic'indentedQ
+  -- ** Unquoted strings
+  , Str'Unquoted (..)
+  , str'tryUnquoted
+  , str'canRenderUnquoted
+  , char'canRenderUnquoted
+  , render'strUnquoted
+  , parse'strUnquoted
+  -- ** String conversions
+  , str'dynamicToStatic
+  , str'staticToDynamic
+  , str'unquotedToDynamic
+  -- ** Indented strings
+  , InStr
+  , inStr'join
+  , inStr'level
+  , inStr'dedent
+  , inStr'trim
+  , render'inStr'1
+  , parse'inStr
+  , parse'inStr'1
+  -- ** Single line of an indented string
+  , InStr'1 (..)
+  , inStr'1'nonEmpty
+  , inStr'1'empty
+  , inStr'1'modifyLevel
+  -------------------------------------------------
+  -- * Lists
+  , List
+  , render'list
+  , parse'list
+  -------------------------------------------------
+  -- * Dicts
+  , Dict (..)
+  , keyword'rec
+  , render'dict
+  , parse'dict
+  , parse'dict'rec
+  , parse'dict'noRec
+  -- ** Dict bindings
+  , DictBinding (..)
+  , render'dictBinding
+  , parse'dictBinding
+  , parse'dictBinding'inherit
+  , parse'dictBinding'eq
+  -- ** Dict lookup (dot)
+  , Dot (..)
+  , expression'applyDots
+  , render'dot
+  , parse'dot'rhs'chain
+  -------------------------------------------------
+  -- * Functions
+  -- ** Lambdas
+  , Lambda (..)
+  , render'lambda
+  , parse'lambda
+  -- ** Function parameters
+  , Param (..)
+  , render'param
+  , parse'param
+  -- ** Dict patterns
+  , DictPattern (..)
+  , DictPattern'1 (..)
+  , render'dictPattern
+  , render'dictPattern'1
+  , parse'dictPattern
+  , parse'dictPattern'start
+  -- ** Function application
+  , Apply (..)
+  , expression'applyArgs
+  , render'apply
+  -------------------------------------------------
+  -- * @let@
+  , Let (..)
+  , keyword'let
+  , keyword'in
+  , render'let
+  , parse'let
+  -- ** @let@ bindings
+  , LetBinding (..)
+  , render'letBinding
+  , parse'letBinding
+  , parse'letBinding'eq
+  , parse'letBinding'inherit
+  -------------------------------------------------
+  -- * @with@
+  , With (..)
+  , keyword'with
+  , render'with
+  , parse'with
+  -------------------------------------------------
+  -- * @inherit@
+  , Inherit (..)
+  , keyword'inherit
+  , render'inherit
+  , parse'inherit
+  -------------------------------------------------
+  -- * Keywords
+  , Keyword
+  , keywords
+  , keywordString
+  , keywordText
+  , parse'keyword
+  -------------------------------------------------
+  -- * Comments and whitespace
+  , keyword'inlineComment
+  , parse'spaces
+  , parse'comment
+  , parse'comment'inline
+  , parse'comment'block
+  -------------------------------------------------
+  -- * Miscellanea
+  , Render
+  , parse'antiquote
+  -------------------------------------------------
   ) where
 
 import Bricks.Expression

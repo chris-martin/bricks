00439358838e92adbfcd73f7e55ceb157bd8506a
Chris Martin <ch.martin@gmail.com>
2017-09-18 01:23:47 -0400

    add a few things to Prelude

--- bricks/src/Bricks/Internal/Prelude.hs
+++ bricks/src/Bricks/Internal/Prelude.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Bricks.Internal.Prelude
 
   ( (<&>)
@@ -16,12 +18,14 @@ module Bricks.Internal.Prelude
   , module Data.Monoid
   , module Data.Semigroup
   , module Data.String
+  , module Data.Traversable
   , module Numeric.Natural
+  , module Prelude
   , module Text.Show
 
   ) where
 
-import Control.Applicative (pure, (*>), (<*), (<*>), (<|>))
+import Control.Applicative (Applicative, pure, (*>), (<*), (<*>), (<|>))
 import Control.Arrow       ((>>>))
 import Control.Monad       ((>>=))
 import Data.Bool           (Bool (False, True), not, (&&), (||))
@@ -29,13 +33,15 @@ import Data.Char           (Char)
 import Data.Either         (Either (..))
 import Data.Eq             (Eq ((/=), (==)))
 import Data.Foldable       (asum, fold, foldMap, foldl, foldl1, foldr, foldr1)
-import Data.Function       (id, ($), (&), (.))
+import Data.Function       (flip, id, ($), (&), (.))
 import Data.Functor        (Functor, fmap, void, ($>), (<$), (<$>))
 import Data.Maybe          (Maybe (Just, Nothing), catMaybes, maybe)
 import Data.Monoid         (Monoid (mappend, mempty))
 import Data.Semigroup      (Semigroup ((<>)))
 import Data.String         (String)
+import Data.Traversable    (traverse)
 import Numeric.Natural     (Natural)
+import Prelude             (undefined)
 import Text.Show           (Show (show, showList, showsPrec), shows)
 
 (<&>) :: Functor f => f a -> (a -> b) -> f b

51456b1f663562b78764c5da0e690c6f6f55382d
Chris Martin <ch.martin@gmail.com>
2017-10-07 22:09:14 -0400

    a failing dict eval test

--- bricks/test/evaluation.hs
+++ bricks/test/evaluation.hs
@@ -50,6 +50,17 @@ prop_str = withTests 1 $ property $ do
   x <- liftIO $ bricks'eval type'string [text|''a${"b"}c''|]
   x === "abc"
 
+{-
+prop_dict :: Property
+prop_dict = withTests 1 $ property $ do
+
+  x <- liftIO $ bricks'eval type'string [text|{ a = "1"; }.a|]
+  x === "1"
+
+  x <- liftIO $ bricks'eval type'string [text|{ "a b" = "1"; }."a b"|]
+  x === "1"
+-}
+
 prop_add :: Property
 prop_add = withTests 1 $ property $ do
 
